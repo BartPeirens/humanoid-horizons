68e282fbafca1b6c40ebc04b065fa21bfbbf58e2
Bart Peirens <bart.peirens@lucatrace.com>
2026-06-25 19:58:19 +0200

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+
+const isGhPages = process.env.GHPAGES === 'true';
 
 export default defineConfig({
 	plugins: [
 		sveltekit({
 			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapterStatic({
+				pages: isGhPages ? 'docs' : 'build',
+				assets: isGhPages ? 'docs' : 'build',
+				fallback: '404.html'
+			}),
+
+			paths: isGhPages ? { base: '/humanoid-horizons' } : {}
 		})
 	]
 });
